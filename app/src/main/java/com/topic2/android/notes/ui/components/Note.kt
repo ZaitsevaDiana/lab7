1641b308648daacce462aaa86820be414a37ed42
ZaitsevaDiana <150392034+ZaitsevaDiana@users.noreply.github.com>
2024-03-24 19:30:57 +0300

    edit / add fona and centrirovanie tekcta,flajka

--- app/src/main/java/com/topic2/android/notes/ui/components/Note.kt
+++ app/src/main/java/com/topic2/android/notes/ui/components/Note.kt
@@ -1,17 +1,24 @@
 package com.topic2.android.notes.ui.components
 
 
+
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.topic2.android.notes.theme.rwGreen
@@ -19,21 +26,28 @@ import com.topic2.android.notes.theme.rwGreen
 
 @Composable
 fun Note() {
-    Row(modifier = Modifier.fillMaxWidth()) {
-        NoteColor(
-            color = rwGreen,
-            size = (40.dp),
-            padding = 4.dp,
-            border = 1.dp
-        )
-        Column(modifier = Modifier.weight(1f)) {
+    val backgroundShape: Shape = RoundedCornerShape(4.dp)
+    Row(
+        modifier = Modifier
+            .padding(8.dp)
+            .shadow(1.dp, backgroundShape)
+            .fillMaxWidth()
+            .heightIn(min = 64.dp)
+            .background(Color.White, backgroundShape)
+        ){
+        Column(modifier = Modifier
+            .weight(1f)
+            .align(Alignment.CenterVertically)
+        ) {
             Text(text = "Заголовок", maxLines = 1)
             Text(text = "Содержимое", maxLines = 1)
         }
         Checkbox(
             checked = false,
             onCheckedChange = { },
-            modifier = Modifier.padding(start = 8.dp)
+            modifier = Modifier
+            .padding(start = 8.dp)
+            .align(Alignment.CenterVertically)
         )
     }
 }
