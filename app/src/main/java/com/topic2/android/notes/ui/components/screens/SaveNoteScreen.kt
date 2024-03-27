2bca4152d3c64ea44be28d1433976f85d661445b
ZaitsevaDiana <150392034+ZaitsevaDiana@users.noreply.github.com>
2024-03-27 23:27:44 +0300

    edit / cozdanie verhnei paneli

--- app/src/main/java/com/topic2/android/notes/ui/components/screens/SaveNoteScreen.kt
+++ app/src/main/java/com/topic2/android/notes/ui/components/screens/SaveNoteScreen.kt
@@ -6,25 +6,42 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.key.Key.Companion.I
+import androidx.compose.ui.res.painterResource
 
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.topic2.android.notes.R
 import com.topic2.android.notes.domain.model.ColorModel
 import com.topic2.android.notes.ui.components.NoteColor
 import com.topic2.android.notes.util.fromHex
 import com.topic2.android.notes.viewmodel.MainViewModel
-import kotlinx.coroutines.CoroutineStart
 
 @Composable
-fun SaveNoteScreen(viewModel: MainViewModel){
-
+fun SaveNoteScreen(
+        viewModel: MainViewModel,
+    onNavigateBack: ()->Unit = {}
+){
+    Scaffold (
+        topBar = {},
+        content = {}
+    )
 }
 
 @Composable
@@ -103,4 +120,75 @@ fun ColorPickerPreview(){
             ColorModel.DEFAULT
         )
     ) { }
+}
+
+@Composable
+private  fun SaveNoteTopAppBar(
+    isEditingMode: Boolean,
+    onBackClick: ()->Unit,
+    onSaveNoteClick: ()->Unit,
+    onOpenColorPickerClick: ()->Unit,
+    onDeleteNoteClick: ()->Unit
+){
+    TopAppBar(
+        title = {
+            Text(
+                text = "Save Note",
+                color = MaterialTheme.colors.onPrimary
+            )
+        },
+        navigationIcon ={
+            IconButton(onClick = onBackClick) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = "Save Note Button",
+                    tint = MaterialTheme.colors.onPrimary
+                )
+            }
+        },
+        actions = {
+            //Save Note action icon
+            IconButton(onClick = onSaveNoteClick) {
+                Icon(
+                    imageVector = Icons.Default.Check,
+                    tint = MaterialTheme.colors.onPrimary,
+                    contentDescription = "Save Note"
+                )
+            }
+            //Open color picker action icon
+            IconButton(onClick = onOpenColorPickerClick) {
+                Icon(
+                    painter = painterResource(
+                        id = R.drawable.ic_baseline_color_lens_24
+                    ),
+                    contentDescription = "Open Color Picker Button",
+                    tint = MaterialTheme.colors.onPrimary
+                )
+            }
+            //Delete action icon
+            if (isEditingMode){
+                IconButton(
+                    onClick = onDeleteNoteClick
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Delete,
+                        contentDescription = "Delete Note Button",
+                        tint = MaterialTheme.colors.onPrimary
+                    )
+                }
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+fun SaveNoteTopAppBarPreview(){
+      SaveNoteTopAppBar(
+          isEditingMode = true,
+          onBackClick = {},
+          onSaveNoteClick = {},
+          onOpenColorPickerClick = {},
+          onDeleteNoteClick={}
+      )
 }
